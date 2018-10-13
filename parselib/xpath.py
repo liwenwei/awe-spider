008c85aa5384c5cb319e2eec2dbb93e9dee1e779
liwenwei <liwenwei06@126.com>
2018-10-13 15:19:24 +0800

    Python LXML lib

---
+++ parselib/xpath.py
@@ -0,0 +1,131 @@
+from lxml import etree
+
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html">first item</a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-inactive"><a href="link3.html">third item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+ </div>
+'''
+
+html = etree.HTML(text)
+result = etree.tostring(html)
+print(result.decode('utf-8'))
+
+# All node(//*)
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//*')
+print(result)
+
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//li')
+print(result)
+
+# Child node(//parent/child)
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//li/a')
+print(result)
+
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//ul/a')
+print(result)
+
+# parent node(..)
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//a[@href="link4.html"]/../@class')
+print(result)
+
+# parent node(parent::)
+result = html.xpath('//a[@href="link4.html"]/parent::*/@class')
+print(result)
+
+# match property
+result = html.xpath('//li[@class="item-0"]')
+print(result)
+
+# get property
+result = html.xpath('//li/a/@href')
+print(result)
+
+# get node text(text())
+result = html.xpath('//li[@class="item-0"]/text()')
+print(result)
+
+result = html.xpath('//li[@class="item-0"]/a/text()')
+print(result)
+
+result = html.xpath('//li[@class="item-0"]//text()')
+print(result)
+
+# 属性多值匹配
+text = '''
+<li class="li li-first"><a href="link.html">first item</a></li>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[@class="li"]/a/text()')
+print(result)
+
+result = html.xpath('//li[contains(@class, "li")]/a/text()')
+print(result)
+
+# 多属性匹配
+text = '''
+<li class="li li-first" name="item"><a href="link.html">first item</a></li>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[contains(@class, "li") and @name="item"]/a/text()')
+print(result)
+
+# 按序选择
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html">first item</a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-inactive"><a href="link3.html">third item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+ </div>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[1]/a/text()')
+print(result)
+result = html.xpath('//li[last()]/a/text()')
+print(result)
+result = html.xpath('//li[position()<3]/a/text()')
+print(result)
+result = html.xpath('//li[last()-2]/a/text()')
+print(result)
+
+# 节点轴选择(XPath Axes)
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html"><span>first item</span></a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-inactive"><a href="link3.html">third item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+ </div>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li[1]/ancestor::*')
+print(result)
+result = html.xpath('//li[1]/ancestor::div')
+print(result)
+result = html.xpath('//li[1]/attribute::*')
+print(result)
+result = html.xpath('//li[1]/child::a[@href="link1.html"]')
+print(result)
+result = html.xpath('//li[1]/descendant::span')
+print(result)
+result = html.xpath('//li[1]/following::*[2]')
+print(result)
+result = html.xpath('//li[1]/following-sibling::*')
+print(result)
